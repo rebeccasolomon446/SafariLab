3170b2129167f8ecba945bb0286beecb3c6b55b6
Rebecca <rebecca.solomon446@gmail.com>
2022-12-01 21:04:46 +0000

    add query for names of staff working in enclosures that are closed for maintenance

--- safari_queries.sql
+++ safari_queries.sql
@@ -1,5 +1,7 @@
 -- safari queries 
 
+-- MVP
+
 -- names of all animals and their enclosures
 SELECT animals.name AS animal_name, enclosures.name FROM animals
 inner join enclosures 
@@ -17,3 +19,13 @@ inner join assignments
 on staff.id = assignments.employee_id
 inner join enclosures
 on enclosures.id = assignments.enclosure_id;
+
+-- Extention 
+-- names of staff working in enclosures that are closed for maintenance
+SELECT staff.name AS staff_name, enclosures.name FROM staff
+inner join assignments 
+on staff.id = assignments.employee_id
+inner join enclosures
+on enclosures.id = assignments.enclosure_id
+WHERE enclosures.closedformaintenance = true;
+

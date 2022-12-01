2c128534116d4be77f93718fccf208ae4c9cda73
Rebecca <rebecca.solomon446@gmail.com>
2022-12-01 17:16:30 +0000

    made a change

--- safari_queries.sql
+++ safari_queries.sql
@@ -1,1 +1,7 @@
 -- safari queries 
+
+-- names of all animals and their enclosures
+SELECT animals.name AS animal_name, enclosures.name FROM animals
+inner join enclosures 
+on enclosures.id = animals.enclosure_id;
+SELECT * FROM enclosures;

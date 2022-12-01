696e80e78b201e95667a8a2961cd0ea7cd219574
Rebecca <rebecca.solomon446@gmail.com>
2022-12-01 20:35:42 +0000

    adds query to get names of all animals in big cat field enclosure

--- safari_queries.sql
+++ safari_queries.sql
@@ -4,3 +4,9 @@
 SELECT animals.name AS animal_name, enclosures.name FROM animals
 inner join enclosures 
 on enclosures.id = animals.enclosure_id;
+
+-- names of all animals in big cat field enclosure
+SELECT animals.name AS animal_name, enclosures.name FROM animals
+inner join enclosures 
+on enclosures.id = animals.enclosure_id
+WHERE enclosures.name = 'big cat field';
